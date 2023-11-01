0ce480756733ebe4821b2bc52938d18fe00b64d8
muhammetemircalisir <emirc7979@gmail.com>
2023-11-01 21:16:32 +0300

    3. commmit

---
+++ phytondersleri2.py
@@ -0,0 +1,12 @@
+print("oyuncu bilgileri kaydetme programı")
+ad = input("oyuncunun adı: ")
+soyad= input("oyuncunun soyadı: ")
+takım=input("oyuncunun takımı:")
+
+bilgiler=[ad,soyad,takım]
+
+print("bilgiler kaydediliyor...")
+
+print("oyuncunun adı:{}\n oyuncunun soyadı: {} \n oyuncunun takımı: {}\n".format(bilgiler[0],bilgiler[1],bilgiler[2]))
+
+print("bilgiler kaydedildi.....")
